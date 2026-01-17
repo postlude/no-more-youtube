d1cedadeda0b281397e0b7e10865a7057428d56a
postlude <seungdeokhan@naver.com>
2026-01-18 01:09:28 +0900

    feat: permanantly redirect from shorts to main

--- background/background.js
+++ background/background.js
@@ -1,11 +1,6 @@
-// 숏츠 URL 감지 및 리다이렉트
+// 숏츠 URL 감지 및 리다이렉트 (항상 차단)
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 	if (tab.url && tab.url.startsWith('https://www.youtube.com/shorts')) {
-		chrome.storage.sync.get(['enabled'], (result) => {
-			const enabled = result.enabled !== undefined ? result.enabled : true;
-			if (enabled) {
-				chrome.tabs.update(tabId, { url: 'https://www.youtube.com/' });
-			}
-		});
+		chrome.tabs.update(tabId, { url: 'https://www.youtube.com/' });
 	}
 }); 
